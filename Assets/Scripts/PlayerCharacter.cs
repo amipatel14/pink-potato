56ba6d293802eb6c8cdbb4eb72fd41a1bade91d0
Ami Patel <Ami.Patel3@iam.local>
2018-09-26 15:20:30 -0500

    WIP Movement
    Added player movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    //private = camelCase public = PascalCase
+    [SerializeField]
+    private int lives = 3;
+
+    [SerializeField]
+    private string name = "Hector";
+    //if they both are the same type use commas
+    [SerializeField]
+    private float jumpHeight = 5, speed = 5;
+
+    private bool hasKey;
+
+    private bool isOnGround;
+
+    private Rigidbody2D rigidbody2DInstance;
+    private float horizontalInput;
 
     // Use this for initialization
     void Start()
     {
-        Debug.Log("Hello"); //how to print to the console in Unity
+        //have to initialize our component
+        rigidbody2DInstance = GetComponent<Rigidbody2D>(); //Get component get the first object in that name and place it in there to use     
+        rigidbody2DInstance.gravityScale = 5;
     }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    private void Update()
     {
-
+        //transform.Translate(0,-.02f,0); don't use translate 'cause we using physics
+        GetInput();
+        Move();
     }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+        private void Move()
+        {
+            rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+        }
+
 }
