ea3f619adf917550e9f7c90f7fcc8597965514f5
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2018-10-16 18:15:03 -0500

    CheckPoint Feedback
    Changed the color, size, and rotation of checkpoint if activated or not.
    Used Update, Start, Time.deltaTime

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,6 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField]
+    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
+
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 1.5f;
+
+    [SerializeField]
+    private Color inactivatedColor, activatedColor;
+
+    private bool isActivated = false;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedColor;
+        if (isActivated)
+            color = activatedColor;
+
+        spriteRenderer.color = color;
+
+        
+    }
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+        if (isActivated)
+            scale = activatedScale;
+
+        transform.localScale = Vector3.one *scale;
+    }
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactivatedRotationSpeed;
+        if (isActivated)
+            rotationSpeed = activatedRotationSpeed;
+
+       transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
